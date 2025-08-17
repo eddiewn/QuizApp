d994b91cb788afb53ac1c155103a519d08cf5541
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-08-17 15:53:34 +0200

    Updated header, created bases to all pages.

--- Header/header.js
+++ Header/header.js
@@ -5,10 +5,10 @@ class reUseHeaderTest extends HTMLElement{
         <header>
             <div id="headerBackground">
                 <nav class="navButtons">
-                    <h2 id="homeButton"><a href="/index.html">Home<a/></h2>
-                    <h2 id="helpButton">Help</h2>
-                    <h2 id="createQuizButton"><a href="/CreateQuiz/createQuiz.html">Create Quiz</a></h2>
-                    <h2 id="profileButton">Profile</h2>
+                    <a id="homeButton" href="/index.html">Home</a>
+                    <a id="helpButton" href="/Help/help.html">Help</a>
+                    <a id="createQuizButton" href="/CreateQuiz/createQuiz.html">Create Quiz</a>
+                    <a id="profileButton" href="/Profile/profile.html">Profile</a>
                 </nav>
             </div>
         </header>
