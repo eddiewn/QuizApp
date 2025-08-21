e59ed6a4c5e72433f2e2d55a1507f3a5672b14c8
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-08-21 19:14:11 +0200

    Now displays question and answers on gamescreen.

--- Game/game.js
+++ Game/game.js
@@ -10,7 +10,37 @@ function game(quiz){
     console.log("Game has started!");
     console.log(quiz)
 
+	let mainTag = document.querySelector("main")
 
+	let questionDiv = document.createElement("div");
+	questionDiv.id = "questionDiv";
+
+	let questionP = document.createElement("p");
+	questionP.id = "questionP";
+
+	let answerDiv = document.createElement("div");
+	answerDiv.id = "answerDiv"
+
+
+	questionP.innerText = quiz[questionTracker].question;
+
+	questionDiv.appendChild(questionP);
+	mainTag.appendChild(questionDiv);
+
+		allAnswers(quiz).forEach(answer => {
+		let answerSquare = document.createElement("div");
+		answerSquare.className = "answerSquare";
+		
+		let answerSquareP = document.createElement("p");
+		answerSquareP.innerText = answer;
+
+		answerSquare.appendChild(answerSquareP)
+		mainTag.appendChild(answerSquare);
+	});
+
+
+	console.log("AM i in loop???");
+	console.log("answer: no");
 }
 
 function shuffle(quiz){
@@ -29,12 +59,21 @@ function shuffle(quiz){
 }
 
 function fisherYatesShuffle(array) {
-  let arr = [...array];
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
-  return arr;
+	let arr = [...array];
+	for (let i = arr.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+  	}
+  	return arr;
+}
+
+function allAnswers(quiz){
+	const allAnswers = [
+	...quiz[questionTracker].fillerValues,
+	...quiz[questionTracker].correctValues
+	];
+
+	return allAnswers;
 }
 
 game(quiz);
