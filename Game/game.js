3b2f324b1315f1042749d957bf4477f31ecbe5eb
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-08-23 10:36:31 +0200

    Added SICK loading bar, broke up long game function into many functions

--- Game/game.js
+++ Game/game.js
@@ -12,50 +12,21 @@ let quizzes = JSON.parse(localStorage.getItem("quizzes"));
 let quiz = quizzes[quizIndex];
 quiz = shuffle(quiz);
 
+// Bug hittad
+// Om "fillerValue" är samma som "correctValue" (även fast det är dumt)
+// Så räknas fillerValue som ett korrekt val, vilket det borde vara. Kanske inte är något concern.
+// Borde kolla specifikt ID istället för content på svar. yikes
+
 function game(quiz){
 	if(questionTracker != quiz.length){
-		console.log("Game has started!");
-		console.log(quiz)
-
-
-		let questionDiv = document.createElement("div");
-		questionDiv.id = "questionDiv";
-
-		let questionP = document.createElement("p");
-		questionP.id = "questionP";
-
-		let answerDiv = document.createElement("div");
-		answerDiv.id = "answerDiv"
-
-		questionP.innerText = quiz[questionTracker].question;
-
-		questionDiv.appendChild(questionP);
-		mainTag.appendChild(questionDiv);
-
-		let answerContainer = document.createElement("div");
-		answerContainer.id = "answerContainer";
-
-		const randomizedAnswers = fisherYatesShuffle(allAnswers(quiz));
-
-		randomizedAnswers.forEach(answer => {
-			console.log(answer);
-			let answerSquare = document.createElement("div");
-			answerSquare.className = "answerSquare";
 		
-			let answerSquareP = document.createElement("p");
-			answerSquareP.innerText = answer;
+		createGameDisplay();
 
-			answerSquare.appendChild(answerSquareP);
-			answerContainer.appendChild(answerSquare);
-		});
-
-		mainTag.appendChild(answerContainer);
-
-	document.querySelectorAll(".answerSquare").forEach(square => {
-		square.addEventListener("click", function() {
-		const isCorrect = quiz[questionTracker].correctValues.some(correctAnswer => 
-			square.textContent == correctAnswer
-			);
+		document.querySelectorAll(".answerSquare").forEach(square => {
+			square.addEventListener("click", function() {
+				const isCorrect = quiz[questionTracker].correctValues.some(correctAnswer => 
+				square.textContent == correctAnswer
+				);
 				playerAnswers.push(square.textContent)
 
 				if(isCorrect){
@@ -63,7 +34,7 @@ function game(quiz){
 				}else{
 
 				};
-
+				
 				document.querySelectorAll(".answerSquare").forEach(square => {
 
 					if(quiz[questionTracker].correctValues.includes(square.textContent)){
@@ -73,16 +44,86 @@ function game(quiz){
 					}
 				})
 
+				createProgressBar();
 
 				setTimeout(() => {
 					questionTracker++
 					mainTag.innerHTML = "";
 					game(quiz);
 
-				}, 1000);
+				}, 3000);
 			});
 		});
 	}else{
+		createEndScreen();
+	}
+}
+
+function createGameDisplay(){
+	let questionDiv = document.createElement("div");
+	questionDiv.id = "questionDiv";
+
+	let questionP = document.createElement("p");
+	questionP.id = "questionP";
+	questionP.innerText = quiz[questionTracker].question;
+
+
+
+	questionDiv.appendChild(questionP);
+	mainTag.appendChild(questionDiv);
+
+	let answerContainer = document.createElement("div");
+	answerContainer.id = "answerContainer";
+
+	let answerDiv = document.createElement("div");
+	answerDiv.id = "answerDiv"
+		
+	const randomizedAnswers = fisherYatesShuffle(allAnswers(quiz));
+
+	randomizedAnswers.forEach(answer => {
+		console.log(answer);
+		let answerSquare = document.createElement("div");
+		answerSquare.className = "answerSquare";
+		
+		let answerSquareP = document.createElement("p");
+		answerSquareP.innerText = answer;
+
+		answerSquare.appendChild(answerSquareP);
+		answerContainer.appendChild(answerSquare);
+	});
+
+	mainTag.appendChild(answerContainer);
+}
+
+function createProgressBar(){
+	const nextQuestion = document.createElement("h1")
+	const progressContainer = document.createElement("div");
+	const progressBar = document.createElement("div");
+	const progress = document.createElement("div");
+				
+	nextQuestion.id = "nextQuestion";
+	nextQuestion.textContent = "Next question coming soon";
+	progressContainer.id = "progressContainer";
+	progressBar.id = "progressBar";
+	progress.id = "progress";
+
+	progressBar.appendChild(progress);
+	progressContainer.appendChild(nextQuestion);
+	progressContainer.appendChild(progressBar);
+	mainTag.appendChild(progressContainer);
+	let increaseWidth = 0;
+
+	const progressMovement = setInterval(()=> {
+		increaseWidth++;
+		if(increaseWidth <= 100){
+			progress.style.width = increaseWidth + "%";
+		}else{
+			clearInterval(progressMovement);
+		}
+	}, 30)
+};
+
+function createEndScreen(){
 		const endScreenDiv = document.createElement("div")
 		endScreenDiv.id = "endScreenDiv";
 		
@@ -96,9 +137,7 @@ function game(quiz){
 		// array.forEach(element => {
 			
 		// });
-	}
 }
-
 
 function shuffle(quiz){
     const shuffledQuestions = fisherYatesShuffle(quiz);
