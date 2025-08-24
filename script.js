79adc387e68c199d9c65d157e1d9539917534a2a
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-08-24 21:52:18 +0200

    test

--- script.js
+++ script.js
@@ -1,76 +1,8 @@
-let growRadius = 0;
-
-window.addEventListener("pageshow", (event) => {
-    if (event.persisted) {
-        const circle = document.createElement("div");
-
-        growRadius = 0;
-        shrinkRadius = 1000;
-        circle.style.setProperty("--r", "0px");
-
-        if (!document.querySelector(".spotlight")) {
-            const circle = document.createElement("div");
-            circle.classList.add("spotlight");
-            circle.style.setProperty("--r", "0px");
-            mainTag.appendChild(circle);
-        }
-    }
-});
-
-    function loadAnimation(){
-        const circle = ensureSpotlight();
-        growRadius = growRadius + 50;
-        circle.style.setProperty("--r", growRadius + "px");
-
-        if (growRadius < 1000) {
-            requestAnimationFrame(loadAnimation);
-        }else{
-            circle.remove();
-        }
-    }
-
+    let growRadius = 0;
     let shrinkRadius = 1000;
-    function deloadAnimation(nextUrl){
-    const circle = ensureSpotlight();
-        mainTag.appendChild(circle);
-        circle.style.setProperty("--r", 1000 + "px");
+    let rafId = null;
 
-        shrinkRadius = shrinkRadius - 40;
-        circle.style.setProperty("--r", shrinkRadius + "px");
-
-        if (shrinkRadius > 0) {
-            requestAnimationFrame(() => deloadAnimation(nextUrl)); 
-        }else{
-            window.location.href=nextUrl
-        }
-    }
-
-    window.addEventListener("beforeunload", () => {
-        growRadius = 0;
-        shrinkRadius = 1000;
-    });
-
-document.addEventListener("DOMContentLoaded", () => {
-    const header = document.getElementById("headerBackground");
-
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > 100) {
-        header.classList.add("scrolled");
-        setTimeout(() => {
-            header.classList.add("wait");
-        },50);
-
-        } else {
-        header.classList.remove("scrolled");
-        setTimeout(() => {
-            header.classList.remove("wait");
-        },50);
-
-        }
-    });
-});
-
-function ensureSpotlight() {
+    function ensureSpotlight() {
     let circle = document.querySelector(".spotlight");
     if (!circle) {
         circle = document.createElement("div");
@@ -78,7 +10,65 @@ function ensureSpotlight() {
         mainTag.appendChild(circle);
     }
     return circle;
-}
+    }
 
+    function loadAnimation() {
+    const circle = ensureSpotlight();
+    circle.style.setProperty("--r", `${growRadius}px`);
+    growRadius += 50;
 
+    if (growRadius < 1000) {
+        rafId = requestAnimationFrame(loadAnimation);
+    } else {
+        circle.remove();
+        rafId = null;
+    }
+    }
 
+    function deloadAnimation(nextUrl) {
+    const circle = ensureSpotlight();
+
+    if (shrinkRadius === 1000) {
+        circle.style.setProperty("--r", "1000px");
+    }
+
+    shrinkRadius -= 40;
+    circle.style.setProperty("--r", `${shrinkRadius}px`);
+
+    if (shrinkRadius > 0) {
+        rafId = requestAnimationFrame(() => deloadAnimation(nextUrl));
+    } else {
+        window.location.href = nextUrl;
+    }
+    }
+
+    window.addEventListener("pageshow", (event) => {
+    if (event.persisted) {
+        if (rafId) cancelAnimationFrame(rafId);
+        growRadius = 0;
+        shrinkRadius = 1000;
+        const circle = ensureSpotlight();
+        circle.style.setProperty("--r", "0px");
+    }
+    });
+
+    window.addEventListener("pagehide", () => {
+    if (rafId) cancelAnimationFrame(rafId);
+    growRadius = 0;
+    shrinkRadius = 1000;
+    });
+
+    document.addEventListener("DOMContentLoaded", () => {
+    loadAnimation();
+
+    const header = document.getElementById("headerBackground");
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+        header.classList.add("scrolled");
+        setTimeout(() => header.classList.add("wait"), 50);
+        } else {
+        header.classList.remove("scrolled");
+        setTimeout(() => header.classList.remove("wait"), 50);
+        }
+    });
+    });
