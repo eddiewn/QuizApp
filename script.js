ee5f026d7fa53f56bf8b9403569906f4668896bb
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-08-24 22:49:49 +0200

    test2

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-    let growRadius = 0;
+const [navEntry] = performance.getEntriesByType("navigation");
+
+if (navEntry && navEntry.type === "back_forward") {
+    location.reload();
+}
+
+
+let growRadius = 0;
+
+function loadAnimation(){
     const circle = document.querySelector(".spotlight");
 
-    function loadAnimation(){
-        growRadius = growRadius + 50;
-        circle.style.setProperty("--r", growRadius + "px");
+    growRadius = growRadius + 50;
+    circle.style.setProperty("--r", growRadius + "px");
 
-        if (growRadius < 1000) {
-            requestAnimationFrame(loadAnimation);
-        }else{
-            circle.remove();
-        }
+    if (growRadius < 1000) {
+        requestAnimationFrame(loadAnimation);
+    }else{
+        circle.remove();
     }
+}
 
-    let shrinkRadius = 1000;
-    function deloadAnimation(nextUrl){
-    const circle = document.createElement("div");
-        circle.classList.add("spotlight");
-        document.body.appendChild(circle);
-        circle.style.setProperty("--r", 1000 + "px");
+let shrinkRadius = 1000;
+function deloadAnimation(nextUrl){
+const circle = document.createElement("div");
+    circle.classList.add("spotlight");
+    document.body.appendChild(circle);
+    circle.style.setProperty("--r", 1000 + "px");
 
-        shrinkRadius = shrinkRadius - 40;
-        circle.style.setProperty("--r", shrinkRadius + "px");
+    shrinkRadius = shrinkRadius - 40;
+    circle.style.setProperty("--r", shrinkRadius + "px");
 
-        if (shrinkRadius > 0) {
-            requestAnimationFrame(() => deloadAnimation(nextUrl)); 
-        }else{
-            window.location.href=nextUrl
-        }
+    if (shrinkRadius > 0) {
+        requestAnimationFrame(() => deloadAnimation(nextUrl)); 
+    }else{
+         window.location.href=nextUrl
     }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     const header = document.getElementById("headerBackground");
