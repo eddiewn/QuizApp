2a4dd1318002855c095a13b5f69689061f59e9b7
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-09-03 10:01:09 +0200

    odobding

--- Game/endscreen.js
+++ Game/endscreen.js
@@ -1,39 +1,37 @@
 export function createEndScreen(mainTag, quiz, playerAnswers) {
     // Räkna ut procenten av rätt o ge en rating berode på det. etc "S+!, A, B, C, D ,E ,F"
-    const endScreenDiv = document.createElement("div");
-    endScreenDiv.id = "endScreenDiv";
+    const endScreenCard = document.createElement("article");
+    endScreenCard.id = "endScreenCard";
 
-    const contentTest = document.createElement("h1");
-    contentTest.textContent = "This is end page";
+    const cardHeader = document.createElement("h1");
 
-    endScreenDiv.appendChild(contentTest);
-    mainTag.appendChild(endScreenDiv);
 
-    //Printa ut frågan + vad spelaren svara
 
     const answerDiv = document.createElement("div");
     answerDiv.id = "answerDiv";
 
+    let answersRight = 0;
     quiz.forEach((question, i) => {
         const playerAnswer = playerAnswers[i];
         const isCorrect = question.correctValues.some(
             (answer) => answer === playerAnswer
         );
-
-        const playerAnswerP = document.createElement("p");
-        const correctAnswerP = document.createElement("p");
-
-        playerAnswerP.textContent = `You answered: ${playerAnswer}`;
-        correctAnswerP.textContent = `Correct answer is: ${question.correctValues}`;
-
-        answerDiv.appendChild(playerAnswerP);
-        answerDiv.appendChild(correctAnswerP);
-        endScreenDiv.appendChild(answerDiv);
-
         if (isCorrect) {
-            // console.log(`${question.question} answered correctly`);
-        } else {
-            // console.log(`${question.question} answered wrongly`);
+            answersRight++;
         }
+
+        endScreenCard.appendChild(answerDiv);
     });
+    let percentage = answersRight / quiz.length * 100;
+
+    if(percentage >= 70){
+        cardHeader.textContent = "Well done!";
+    }else{
+        cardHeader.textContent = "Quiz Done!"
+    }
+
+    console.log(percentage + "%");
+
+    endScreenCard.appendChild(cardHeader);
+    mainTag.appendChild(endScreenCard);
 }
